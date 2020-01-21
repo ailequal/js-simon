7330b970b379269fec9f3ff970f181fa1acb2093
ailequal <37016865+ailequal@users.noreply.github.com>
2020-01-21 16:33:27 +0100

    Change some var naming scheme

--- js/script.js
+++ js/script.js
@@ -1,27 +1,41 @@
 // code
 $(document).ready(function () {
   // creating array that stores the numbers (without repetition)
-  var arrayNumber = [];
-  var number = 0;
+  var arrayBot = [];
+  var numberBot = 0;
   for (var i = 0; i < 5; i++) {
     do {
-      number = getRandomNumber(1, 100);
-    } while (scanArray(arrayNumber, number));
-    arrayNumber.push(number);
+      numberBot = getRandomNumber(1, 100);
+    } while (scanArray(arrayBot, numberBot));
+    arrayBot.push(numberBot);
   }
 
-  // display an alert with 5 random numbers
-  var alertNumber = '';
-  for (var i = 0; i < arrayNumber.length; i++) {
-    alertNumber = alertNumber + ' ' + arrayNumber[i];
+  // display an alert with 5 random numberBots
+  var alertBot = '';
+  for (var i = 0; i < arrayBot.length; i++) {
+    alertBot = alertBot + ' ' + arrayBot[i];
   }
-  alert('Try to memorize this numbers: ' + alertNumber);
+  alert('Try to memorize this numbers: ' + alertBot);
 
-  // start a timer for 30 seconds
+  // start a timer for 30 seconds and then start the function
+  // that will ask the user to insert the 5 numbers (in order??)
+  var arrayUser = [];
+  arrayUser = setTimeout(function () {
+    for (var i = 0; i < 5; i++) {
+      do {
+  
+      } while (true);
+      // aske 3 times number, if between 1 and 100 push, otherwise ask again
+      // only between 1 and 100
+      // not same number
+    }
+    return arrayUser;
+  }, 1000);
 
-  // ask the user to insert the 5 numbers (in order??)
+  // compare the inserted numbers with the arrayBot
 
   // display the results (how many and which one were correct)
+
 });
 
 
@@ -41,6 +55,14 @@ function scanArray(array, item) {
       return true;
     }
     i++;
+  }
+  return false;
+}
+
+// check that a number is between min and max
+function checkMinMax(min, max, number) {
+  if (number >= min && number <= max) {
+    return true;
   }
   return false;
 }
